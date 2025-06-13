9bc82a5f015c510f169037420670cc5e1bda9aaf
Mani Chandan Gujja <gujjamanichandan@gmail.com>
2025-06-13 07:56:28 -0400

    Refactor ResultModel and TimeChallenge components for improved dialog handling and state management

--- src/components/ResultModel.jsx
+++ src/components/ResultModel.jsx
@@ -1,26 +1,31 @@
-import { useImperativeHandle , useRef } from "react"
+import { useImperativeHandle, useRef } from "react";
 
-export default function ResultModel({ ref, targetTime, result }) {
+export default function ResultModel({ ref, targetTime, timeRemaining , onReset }) {
+    const dialog = useRef();
 
-    const dialog = useRef()
+    const formatedTimeRemaining = (timeRemaining / 1000).toFixed(2);
+    const userLost = timeRemaining <= 0;
+    const score = Math.round((1- timeRemaining/(targetTime * 1000))* 100); 
 
-	useImperativeHandle(ref, () => ({
-		open() {
-			dialog.current.showModal();
-		}
-	}));
-	return (
-		<dialog ref={dialog} className="result-modal" open>
-			<h2>You {result}</h2>
-			<p>
-				The target time was <strong>{targetTime} seconds.</strong>
-			</p>
-			<p>
-				you stoped at <strong>x seconds</strong>
-			</p>
-			<form method="dialog">
-				<button>Close</button>
-			</form>
-		</dialog>
-	)
+    useImperativeHandle(ref, () => ({
+        open() {
+            dialog.current.showModal();
+        },
+    }));
+
+    return (
+        <dialog ref={dialog} className="result-modal" onClose={onReset}>
+           { userLost && <h2>You lost</h2>}
+           { !userLost && <h2>Your score: {score}</h2>}
+            <p>
+                The target time was <strong>{targetTime} seconds.</strong>
+            </p>
+            <p>
+                You stopped at <strong>{formatedTimeRemaining} seconds</strong>
+            </p>
+            <form method="dialog" onSubmit={onReset}>
+                <button>Close</button>
+            </form>
+        </dialog>
+    );
 }
--- src/components/TimeChallenge.jsx
+++ src/components/TimeChallenge.jsx
@@ -6,35 +6,46 @@ export default function TimeChallenge({ title, targetTime }) {
 	const timer = useRef()
     const dialog = useRef()
 
-	const [timerExpired, setTimerExpired] = useState(false)
-	const [timeStart, setTimeStart] = useState(false)
+	const [timeRemaining, setTimeRemaining] = useState(targetTime* 1000);
+
+    const timeIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;  
+
+    if(timeRemaining <= 0){
+        clearTimeout(timer.current);
+       
+        dialog.current.open();
+    }
+
+    function handleReset(){
+            setTimeRemaining(targetTime * 1000);
+    }
 
 	function handleStart() {
-		timer.current = setTimeout(() => {
-			setTimerExpired(true);
-            dialog.current.open();
-		}, targetTime * 1000)
-		setTimeStart(true)
+		timer.current = setInterval(() => {
+			setTimeRemaining(prevTimeRemaining => prevTimeRemaining-10)
+		}, 10)
+		
 	}
 
 	function handleStop() {
-		clearTimeout(timer.current)
+        dialog.current.open();
+		clearInterval(timer.current);
 	}
 
 	return (<>
-     <ResultModel ref={dialog} targetTime={targetTime} result="lose"/>
+     <ResultModel ref={dialog} targetTime={targetTime} timeRemaining={timeRemaining} onReset={handleReset}/>
 		<section className="challenge">
 			<h2>{title}</h2>
 			<p className="challenge-time">
 				{targetTime} second{targetTime > 1 ? 's' : ''}{' '}
 			</p>
 			<p>
-				<button onClick={timeStart ? handleStop : handleStart}>
-					{timeStart ? 'stop' : 'start'} challenge
+				<button onClick={timeIsActive ? handleStop : handleStart}>
+					{timeIsActive ? 'stop' : 'start'} challenge
 				</button>
 			</p>
-			<p className={timeStart ? 'active' : undefined}>
-				{timeStart ? 'Time is Running...' : 'Time is stoped...'}
+			<p className={timeIsActive ? 'active' : undefined}>
+				{timeIsActive ? 'Time is Running...' : 'Time is stoped...'}
 			</p>
 		</section>
         </>
